53cc4b8a8edad61f192a64bcd1ff1628ffac7980
478963019 <478963019_1938804310@cn-shenzhen-e-198-2.wuying.local>
2023-02-27 11:14:37 +0800

    first commit

---
+++ hospital-api/src/test/java/com/example/hospital/api/HospitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospital.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
